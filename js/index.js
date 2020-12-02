f1f2d62264bede982d04b0c1188afe2bff0e86a5
andres <afmartinez099@misena.edu.co>
2020-12-02 18:53:24 -0500

    Proyecto JS

---
+++ js/index.js
@@ -0,0 +1,115 @@
+//Variables
+
+console.log("Hola desde index.js")
+
+var x = 10
+
+var y = 55.55
+
+console.log("X="+x+" Y="+y)
+
+var nombre="Pedro"
+
+var text= "Mi nombre es Pedro Perez"
+
+console.log(nombre)
+
+console.log(text)
+
+document.write(text)
+
+var x= 19
+
+console.log(x)
+
+var text2= "Hola a todos!Soy Pedro un estudiante que aprende a programar!.";
+
+console.log(text2)
+
+//Operaciones Aritmeticas
+
+var x1 = 10+5
+
+console.log(x1);
+
+var x2=10;
+
+var y2=x2+5+22+1000+50;
+
+var z=x1*2;
+
+var y3=y2/4;
+
+var d=26%6
+
+var text3 = " y tengo  "
+
+document.write(text3)
+
+document.write(x1+z)
+
+console.log(y2)
+
+//Operadores matematicos
+
+var x3 = 9
+
+var b = --x3
+
+console.log(b)
+
+//Operadores de comparacion
+
+var c = 96 == 8
+
+var C = "hola" > "HOLA"
+
+console.log (c)
+
+console.log(C)
+
+//Operadores lógicos o booleanos
+
+var logica = (4>2)&&(10<15) 
+
+console.log(logica)
+
+//Operadores de cadena de texto
+
+var op= "Mi nombre es Pedro Pérez"
+
+var op2= "aprendo JavaScript”"
+
+document.write(op + op2);
+
+
+var l = "50";
+
+var m="200";
+
+console.log(l+m);
+
+//CONDICIONALES
+
+var num1 = 7;      
+
+var num2 = 10;
+
+if (num1 > num2) {alert
+
+("Este es mi primer condicional")}
+
+else{
+
+    alert("Este es mi otro condicional")};
+
+    var course = 3;
+
+    if (course ==1) {document.write
+
+        ("<h1>TUTORIAL DE CSS</h1>");}
+
+else if (course == 2) {document.write("<h1>TUTORUAL DE CSS")}
+
+else {document.write("<h1>TUTORIAL DE JAVASCRIPT</h1>")}
+
